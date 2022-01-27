c2f81a82ef7ddd50b6430fc787bb999c4cd01139
PavelAlkhin <unicon2000@gmail.com>
2022-01-27 16:03:44 +0200

    Update CustomerService.java

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -8,17 +8,11 @@ public class CustomerService {
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
-//    Map<Customer, String> customers = new TreeMap<>(new CustomerComporator());
-
     NavigableMap<Customer, String> customers = new TreeMap<Customer, String>((o1, o2) -> Long.compare(o1.getScores(), o2.getScores()));
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-//        Map<Customer, String> customers2 = copyCutomers();
-
-        Set<Map.Entry<Customer, String>> set = customers.entrySet();
-        Iterator<Map.Entry<Customer, String>> iterator = set.iterator();
-        Map.Entry mapEntry = (Map.Entry) iterator.next();
+        Map.Entry<Customer, String> mapEntry = customers.firstEntry();
         Customer cst = (Customer) mapEntry.getKey();
 
         Map.Entry<Customer, String> entry =
@@ -32,22 +26,16 @@ public class CustomerService {
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
 
-        Set<Map.Entry<Customer, String>> set = customers.entrySet();
-        Iterator<Map.Entry<Customer, String>> iterator = set.iterator();
-
         Map.Entry<Customer, String> result = null;
 
-        while (iterator.hasNext()) {
-            Map.Entry mapEntry = (Map.Entry) iterator.next();
-            Customer cstmEntry = (Customer) mapEntry.getKey();
-            if (cstmEntry.getScores() > customer.getScores()) {
-                Customer cst = (Customer) mapEntry.getKey();
-                result = new AbstractMap.SimpleEntry<>(
-                        new Customer(cst.getId(), cst.getName(), cst.getScores()),
-                        (String) mapEntry.getValue()
-                );
-                break;
-            }
+        Map.Entry<Customer, String> customerStringEntry = customers.higherEntry(customer);
+
+        if (customerStringEntry != null) {
+            Customer cst = (Customer) customerStringEntry.getKey();
+            result = new AbstractMap.SimpleEntry<>(
+                    new Customer(cst.getId(), cst.getName(), cst.getScores()),
+                    (String) customerStringEntry.getValue()
+            );
         }
         return result;
     }
