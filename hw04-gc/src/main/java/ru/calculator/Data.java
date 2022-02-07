0a7424a3ebd3a50afd0958734c73c02c9c604946
PavelAlkhin <unicon2000@gmail.com>
2022-02-07 19:45:06 +0200

    hw04-gc

---
+++ hw04-gc/src/main/java/ru/calculator/Data.java
@@ -0,0 +1,18 @@
+package ru.calculator;
+
+public class Data {
+    private final Integer value;
+//    private Integer value;
+
+    public Data(Integer value) {
+        this.value = value;
+    }
+
+//    public void setValue(Integer value) {
+//        this.value = value;
+//    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
