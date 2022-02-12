21a8f5f520197deac1615022e847293828b090c5
PavelAlkhin <unicon2000@gmail.com>
2022-02-12 09:57:07 +0200

    еще секунда из-за того что вытащил создание объекта из цикла

--- hw04-gc/src/main/java/ru/calculator/Data.java
+++ hw04-gc/src/main/java/ru/calculator/Data.java
@@ -1,16 +1,19 @@
 package ru.calculator;
 
 public class Data {
-    private final Integer value;
-//    private Integer value;
+//    private final Integer value;
+    private Integer value;
 
     public Data(Integer value) {
         this.value = value;
     }
 
-//    public void setValue(Integer value) {
-//        this.value = value;
-//    }
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Data() {
+    }
 
     public Integer getValue() {
         return value;
