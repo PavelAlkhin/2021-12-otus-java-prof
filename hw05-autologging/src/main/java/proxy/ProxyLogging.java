b672474a08fa353fc41ce7b0bc74b61c6dd257da
PavelAlkhin <unicon2000@gmail.com>
2022-02-15 13:08:31 +0200

    Update ProxyLogging.java
    Добил)

--- hw05-autologging/src/main/java/proxy/ProxyLogging.java
+++ hw05-autologging/src/main/java/proxy/ProxyLogging.java
@@ -12,72 +12,79 @@ import java.util.Map;
 
 public class ProxyLogging {
     public static CatsCreator createMyClass() {
-        InvocationHandler handler = new DemoInvocationHandler(new CatsCreatorImpl());
+        Class<?>[] classes = {CatsCreator.class};
+        InvocationHandler handler = new DemoInvocationHandler(new CatsCreatorImpl(), classes[0]);
         return (CatsCreator) Proxy.newProxyInstance(ProxyLogging.class.getClassLoader(),
-                new Class<?>[]{CatsCreator.class}, handler);
+                classes, handler);
     }
 
     static class DemoInvocationHandler implements InvocationHandler {
-        private final CatsCreatorImpl myClass;
-        private final Method[] declaredMethods;
-        private final Map<Method, String> loggingMethods;
+        private CatsCreatorImpl myClass;
+        private Map<String, Method> methods;
 
-        DemoInvocationHandler(CatsCreatorImpl myClass) {
+        DemoInvocationHandler(CatsCreatorImpl myClass, Class<?> classInterface) {
             this.myClass = myClass;
-            this.declaredMethods = myClass.getClass().getDeclaredMethods();
-            this.loggingMethods = new HashMap<>();
+            this.methods = new HashMap<>();
 
-            for (Method m : declaredMethods) {
-                if (m.isAnnotationPresent(Log.class)) {
-                    this.loggingMethods.put(m, m.getName());
+            Method[] declaredMethods = myClass.getClass().getDeclaredMethods();
+            Method[] intefMethods = classInterface.getDeclaredMethods();
+            for (Method mImpl : declaredMethods) {
+                if (mImpl.isAnnotationPresent(Log.class)) {
+
+                    Class<?>[] mParameterTypes = mImpl.getParameterTypes();
+                    int length = mParameterTypes.length;
+                    boolean ok = true;
+                    boolean isFoundMethod = false;
+
+                    for (Method mIntrf : intefMethods) {
+
+                        if (mIntrf.getName() == mImpl.getName()) {
+                            Class[] intefParameterTypes = mIntrf.getParameterTypes();
+                            for (int i = 0; i < length; i++) {
+                                try {
+                                    Class aClassImpl = mParameterTypes[i];
+                                    Class aClassIntrf = intefParameterTypes[i];
+                                    if (!aClassImpl.getCanonicalName().equals(aClassIntrf.getCanonicalName())) {
+                                        ok = false;
+                                        continue;
+                                    }
+                                    isFoundMethod = true;
+                                } catch (Exception ex) {
+                                    ok = false;
+                                    continue;
+                                }
+                            }
+                            if (isFoundMethod && ok) {
+                                this.methods.put(generateClasssName(mIntrf), mIntrf);
+                            }
+                        }
+
+                    }
+
                 }
             }
+        }
+
+        private String generateClasssName(Method m) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(m.getName());
+            stringBuilder.append("_");
+            for (Class cls : m.getParameterTypes()) {
+                stringBuilder.append(cls.getCanonicalName());
+                stringBuilder.append("_");
+            }
+            return stringBuilder.toString();
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-            boolean logging = isLogging(method, args, false);
-
-            if (logging) {
+            Method m = methods.get(generateClasssName(method));
+            if (m != null) {
                 System.out.println("executed method:" + method.getName() + ", param:" + args.length);
+                return m.invoke(myClass, args);
             }
-
             return method.invoke(myClass, args);
-        }
-
-        private boolean isLogging(Method method, Object[] args, boolean logging) {
-            for(Map.Entry<Method, String> entry : loggingMethods.entrySet()) {
-                boolean ok = true;
-                String mName = entry.getValue();
-                if (!mName.equals(method.getName())){
-                    continue;
-                }
-                Method m = entry.getKey();
-                for (int i = 0; i < args.length; i++) {
-                    Object argObj = args[i];
-                    Class<?> aClassImpl = argObj.getClass();
-                    if (!compareMethodParam(m, i, aClassImpl)) {
-                        ok = false;
-                    }
-                }
-                if (ok) {
-                    logging = true;
-                }
-            }
-            return logging;
-        }
-
-        private boolean compareMethodParam(Method m, int i, Class<?> aClassImpl) {
-            try {
-                Class<?>[] aClassImplMethod = m.getParameterTypes();
-                if (aClassImpl.getCanonicalName().equals(aClassImplMethod[i].getCanonicalName())) {
-                    return true;
-                }
-            } catch (Exception e) {
-                return false;
-            }
-            return false;
         }
 
         @Override
